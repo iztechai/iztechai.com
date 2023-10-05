4af3208e5f232be4d37d0dfcacf8f512e87dec0c
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-10-05 21:16:30 +0300

    feat: add cloudflare SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
