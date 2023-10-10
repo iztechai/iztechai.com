d048494b1b1d87a703aba61e12cb201e5e5abed2
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-10-10 16:35:20 +0300

    feat: config robots-txt, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
+import robotsConfig from './robots-txt.config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  site: 'https://iztechai.com',
+  integrations: [tailwind(), robotsTxt(robotsConfig), sitemap()],
 });
