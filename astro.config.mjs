4e55a0cd03dd2f27f3cff24c33670fd632bd1032
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-10-12 03:10:50 +0300

    feat: mobile v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import robotsTxt from 'astro-robots-txt';
 import robotsConfig from './robots-txt.config';
 import sitemap from '@astrojs/sitemap';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://iztechai.com',
-  integrations: [tailwind(), robotsTxt(robotsConfig), sitemap()],
+  integrations: [tailwind(), robotsTxt(robotsConfig), sitemap(), react()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#0F151A',
+        foreground: '#DEE5E5',
+        contrast: '#1C3144',
+        iztechred: '#9a0e20',
+        blurple: '#5865F2',
+        linkedin: '#0077B5',
+        youtube: '#FF0000',
+      },
+    },
+  },
+  plugins: [],
+};
