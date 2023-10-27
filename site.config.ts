31011d052d7b243c7a91242a320a81d33792c256
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-10-27 12:15:17 +0300

    chore: revert git branch check, enable indexing

--- site.config.ts
+++ site.config.ts
@@ -1,9 +1,3 @@
-import { execSync } from 'child_process';
-
-function getGitBranch(): string {
-  return execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-}
-
 export default {
-  disableIndexing: getGitBranch() !== 'main',
+  disableIndexing: false,
 };
